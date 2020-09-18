bd71bbc64466901a0501614956848bd483e0c43d
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-09-18 11:38:01 -0300

    Basic Structure

---
+++ app/config/db.example.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    user: 'USER',//Your user
+    password: 'PASSWORD',// Your password
+    server: 'SERVER', // Your server
+    database: 'DB NAME', // Your database name
+    "options": {
+      "encrypt": false,
+      "enableArithAbort": true
+      }
+  };
---
+++ app/routes/users.routes.js
@@ -0,0 +1,21 @@
+module.exports = app => {
+    const users = require("../controllers/Users.controller.js");
+  
+    // Cria um novo aluno
+    /*app.post("/users", users.create);
+  
+    // Pega todos os alunos na tabela
+    app.get("/users", users.findAll);
+  
+    // Pega só um aluno da tabela
+    app.get("/users/:id", users.findOne);
+  
+    // Altera todos os alunos da tabela
+    app.put("/users/:id", users.update);
+  
+    // Deleta os alunos com ra
+    app.delete("/users/:id", users.delete);
+  
+    // Deleta todas as disciplinas
+    app.delete("/users", users.deleteAll);*/
+  };
