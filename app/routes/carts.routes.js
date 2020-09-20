c405bcc03139c05b6137b5098ec470ebff83a767
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-09-20 16:36:37 -0300

    Adjustments

--- app/routes/carts.routes.js
+++ app/routes/carts.routes.js
@@ -1,21 +1,13 @@
 module.exports = app => {
     const carts = require("../controllers/Carts.controller.js");
-  
-    // Cria um novo aluno
+
     app.post("/carts", carts.create);
   
-    // Pega todos os alunos na tabela
-    app.get("/carts", carts.findAll);
-  
-    // Pega só um aluno da tabela
     app.get("/carts/:id", carts.findOne);
-  
-    // Altera todos os alunos da tabela
+
     app.put("/carts/:id", carts.update);
-  
-    // Deleta os alunos com ra
+
     app.delete("/carts/:id", carts.delete);
-  
-    // Deleta todas as disciplinas
+
     app.delete("/carts", carts.deleteAll);
   };
