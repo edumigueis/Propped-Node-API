97833006cbe1b7e7f3b4c84c3768d7ddcfef134c
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-09-20 16:38:56 -0300

    Adjustments

--- app/routes/orders.routes.js
+++ app/routes/orders.routes.js
@@ -1,21 +1,15 @@
 module.exports = app => {
     const orders = require("../controllers/Orders.controller.js");
-  
-    // Cria um novo aluno
+
     app.post("/orders", orders.create);
-  
-    // Pega todos os alunos na tabela
+
     app.get("/orders", orders.findAll);
-  
-    // Pega só um aluno da tabela
+
     app.get("/orders/:id", orders.findOne);
-  
-    // Altera todos os alunos da tabela
+
     app.put("/orders/:id", orders.update);
-  
-    // Deleta os alunos com ra
+
     app.delete("/orders/:id", orders.delete);
-  
-    // Deleta todas as disciplinas
+
     app.delete("/orders", orders.deleteAll);
   };
--- app/routes/rating.routes.js
+++ app/routes/rating.routes.js
@@ -1,21 +1,15 @@
 module.exports = app => {
     const rating = require("../controllers/Rating.controller.js");
   
-    // Cria um novo aluno
     app.post("/rating", rating.create);
   
-    // Pega todos os alunos na tabela
     app.get("/rating", rating.findAll);
   
-    // Pega só um aluno da tabela
     app.get("/rating/:id", rating.findOne);
   
-    // Altera todos os alunos da tabela
     app.put("/rating/:id", rating.update);
   
-    // Deleta os alunos com ra
     app.delete("/rating/:id", rating.delete);
   
-    // Deleta todas as disciplinas
     app.delete("/rating", rating.deleteAll);
   };
