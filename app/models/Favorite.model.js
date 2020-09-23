2038121b7948ccd40846ba461022060e27988671
digo-smithh <smithrodrigues08@gmail.com>
2020-09-23 09:29:43 -0300

    edit

--- app/models/Favorite.model.js
+++ app/models/Favorite.model.js
@@ -68,7 +68,6 @@ Favorite.updateByCode = (cod, favorite, result) => {
       }
 
       if (res.affectedRows == 0) {
-        // não achou a favorite com esse cod
         result(
           {
             kind: "not_found",
@@ -96,7 +95,6 @@ Favorite.remove = (code, result) => {
       }
 
       if (res.affectedRows == 0) {
-        // não achou a favorite com esse cod
         result(
           {
             kind: "not_found",
