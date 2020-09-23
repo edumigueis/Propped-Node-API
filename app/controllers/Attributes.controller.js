4d838ae9378e9fc2df0699a859ef9ae06bd9335d
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-09-23 08:47:49 -0300

    More Models and Controllerslers

--- app/controllers/Attributes.controller.js
+++ app/controllers/Attributes.controller.js
@@ -1,116 +1,93 @@
-/*const Aluno = require("../models/Aluno.model.js");
-const Resultado = require("../models/Resultado.model.js");
-const Matricula = require("../models/Matricula.model.js");
+const Attribute = require("../models/Attribute.model.js");
 
-// Cria e salva um novo aluno
 exports.create = (req, res) => {
   // Validate request
   if (!req.body) {
     res.status(400).send({
-      message: "Conteúdo não pode estar vazio"
+      message: "Empty params",
     });
   }
 
-  // Cria um Aluno
-  const aluno = new Aluno({
-    ra: req.body.RA,
-    nome: req.body.Nome,
+  const attribute = new Attribute({
+    code_attribute: req.body.code_attribute,
+    name_attribute: req.body.name_attribute
   });
 
-  // Salva Aluno no banco de dados
-  Aluno.create(aluno, (err, data) => {
+  Attribute.create(attribute, (err, data) => {
     if (err)
       res.status(500).send({
-        message: err.message || "Erro ao criar aluno."
+        message: err.message || "Error while trying to create attribute.",
       });
     else res.send(data.recordset);
   });
 };
 
-// Pega todos os alunos do banco de dados
 exports.findAll = (req, res) => {
-  Aluno.getAll((err, data) => {
+  Attribute.getAll((err, data) => {
     if (err)
       res.status(500).send({
-        message: err.message || "Erro ao buscar alunos."
+        message: err.message || "Error while searching for attributes.",
       });
     else res.send(data.recordset);
   });
 };
 
-// Achar aluno com ra especifico
 exports.findOne = (req, res) => {
-  Aluno.findByRA(req.params.ra, (err, data) => {
+  Attribute.findByCode(req.params.code_attribute, (err, data) => {
     if (err) {
       if (err.kind === "not_found") {
         res.status(404).send({
-          message: `Não foi possível encontar o aluno com ra ${req.params.ra}.`
+          message: `Attribute with the code ${req.params.code_attribute} wasn't found.`,
         });
       } else {
         res.status(500).send({
-          message: "Erro ao busar o aluno com ra " + req.params.ra
+          message: "Error while searching for attribute with the code " + req.params.code_attribute,
         });
       }
     } else res.send(data);
   });
 };
 
-// Altera o aluno com ra específico
 exports.update = (req, res) => {
   // Validate Request
   if (!req.body) {
     res.status(400).send({
-      message: "Conteúdo não pode estar vazio!"
+      message: "Body of request can not be empty.",
     });
   }
 
-  Aluno.updateByRA(
-    req.params.ra,
-    new Aluno(req.body),
-    (err, data) => {
-      if (err) {
-        if (err.kind === "not_found") {
-          res.status(404).send({
-            message: `Não foi possível encontrar aluno com ra ${req.params.ra}.`
-          });
-        } else {
-          res.status(500).send({
-            message: "Erro ao atualizar aluno com ra " + req.params.ra
-          });
-        }
-      } else res.send(data.recordset);
-    }
-  );
-};
-
-// Deleta aluno com ra especifico
-exports.delete = (req, res) => {
-  Aluno.remove(req.params.ra, (err, data) => {
+  Attribute.updateByCode(req.params.code_attribute, new Attribute(req.body), (err, data) => {
     if (err) {
       if (err.kind === "not_found") {
         res.status(404).send({
-          message: `Não foi possível encontrar aluno com ra ${req.params.alunoId}.`
+          message: `Attribute with the code ${req.params.code_attribute} wasn't found.`,
         });
       } else {
         res.status(500).send({
-          message: "Erro ao deletar aluno com ra " + req.params.alunoId
+          message: "Error when trying to update attribute with the following code: " + req.params.code_attribute,
         });
       }
-    } else res.send({
-      message: `Aluno foi deletado com sucesso!`
-    });
+    } else res.send(data.recordset);
   });
 };
 
-// Delete todos os alunos do banco
-exports.deleteAll = (req, res) => {
-  Aluno.removeAll((err, data) => {
-    if (err)
-      res.status(500).send({
-        message: err.message || "Algum erro ocorreu ao deletar os alunos"
+
+exports.delete = (req, res) => {
+  Attribute.remove(req.params.code_attribute, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Attribute with the code ${req.params.code_attribute} wasn't found.`,
+        });
+      } else {
+        res.status(500).send({
+          message: "Error when trying to update attribute with the following code: " + req.params.code_attribute,
+        });
+      }
+    } else{
+      res.send({
+        message: `Attribute has been deleted succesfully!`,
       });
-    else res.send({
-      message: `Todos os alunos deletados com sucesso!`
-    });
+    }
   });
-};*/
+};
---
+++ app/models/Attribute.model.js
@@ -0,0 +1,111 @@
+const sql = require("./db.js");
+
+const Attribute = function (attribute) {
+    this.code_attribute = attribute.code_attribute;
+    this.name_attribute = attribute.name_attribute;
+};
+
+Attribute.create = (newAttribute, result) => {
+  sql.query(
+    `INSERT INTO Attribute_Propped VALUES('${newAttribute.code_attribute}','${newAttribute.name_attribute}')`,
+    (err, res) => {
+      if (err) {
+        result(err, null);
+        return;
+      }
+      result(null, {
+        ...newAttribute,
+      });
+    }
+  );
+};
+
+Attribute.findByCode = (attributeCODE, result) => {
+  sql.query(
+    `SELECT * FROM Attribute_Propped WHERE code_attribute = '${attributeCODE}'`,
+    (err, res) => {
+      if (err) {
+        result(err, null);
+        return;
+      }
+
+      if (res.recordset.length > 0) {
+        result(null, res);
+        return;
+      }
+
+      result(
+        {
+          kind: "not_found",
+        },
+        null
+      );
+    }
+  );
+};
+
+Attribute.getAll = (result) => {
+  sql.query("SELECT * FROM Attribute_Propped", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log("attributes: ", res);
+    result(null, res.recordset);
+  });
+};
+
+Attribute.updateByCode = (cod, attribute, result) => {
+  attribute.code_attribute = cod;
+  sql.query(
+    `UPDATE Attribute_Propped SET name_attribute = ${attribute.name_attribute} WHERE code_attribute = '${cod}'`,
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+
+      if (res.affectedRows == 0) {
+        result(
+          {
+            kind: "not_found",
+          },
+          null
+        );
+        return;
+      }
+
+      result(null, {
+        code_attribute: cod,
+        ...attribute,
+      });
+    }
+  );
+};
+
+Attribute.remove = (code, result) => {
+  sql.query("DELETE FROM Attribute_Propped WHERE code_attribute = '" + code + "'", 
+  (err, res) => {
+    if (err) {
+      result(null, err);
+      return;
+    }
+
+    if (res.affectedRows == 0) {
+      result(
+        {
+          kind: "not_found",
+        },
+        null
+      );
+      return;
+    }
+
+    result(null, res);
+  });
+};
+
+module.exports = Attribute;
